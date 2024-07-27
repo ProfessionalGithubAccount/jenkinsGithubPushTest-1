96437f5d586b05e09f70a034aa47d391eef3185e
G kausthub <KausthubRao.Garikapati@ust.com>
2024-07-27 17:38:01 +0530

    save

---
+++ src/main/java/com/ust/jenkinsTest/JenkinsGithubPushTest1Application.java
@@ -0,0 +1,13 @@
+package com.ust.jenkinsTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithubPushTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsGithubPushTest1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ust/jenkinsTest/JenkinsGithubPushTest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ust.jenkinsTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsGithubPushTest1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
